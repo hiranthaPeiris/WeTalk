69b43d4d217b02c6d85a5c02b96e683885819258
hiranthaPeiris <hiranthapeiris404@gmail.com>
2019-04-12 01:37:07 +0530

    initial server file request

--- server.js
+++ server.js
@@ -1,8 +1,20 @@
 var http = require('http');
+var fs = require('fs');
 
-http.createServer(function (req,res) {
-    console.log("User made a request "+ req.url);
-    res.writeHead(200,{'content-Type':'text/plain'});
-    res.write('Hello world');
-    res.end();
+//404_response
+function send404Response(response) {
+    response.writeHead(404, { 'Content-Type': 'text/plain' });
+    response.write('Error 404: page not found');
+    response.end();
+}
+
+http.createServer(function (req, res) {
+    console.log("User made a request " + req.url);
+    if (req.method == 'GET' && req.url == '/') {
+        res.writeHead(200, { 'Content-Type': 'text/html' });
+        fs.createReadStream("./index.html").pipe(res);
+    } else {
+        send404Response(res);
+    }
 }).listen(8080);
+console.log("server is running..");
