1139c388dfc542d63fc0d751c4c2149ddf18cb25
hiranthaPeiris <hiranthapeiris404@gmail.com>
2019-04-17 14:49:19 +0530

    vanilla javascripts added

---
+++ public/javascripts/socket.js
@@ -0,0 +1,21 @@
+//Front-end socket.io connections
+var socket = io.connect('http://localhost:3000');
+socket.on('news', function (data) {
+    console.log(data);
+    socket.emit('my other event', { my: 'data' });
+});
+
+//Query DOM
+var handle = document.getElementById('handle'),
+    message = document.getElementById('message'),
+    sendBtn = document.getElementById('send'),
+    output = document.getElementById('output');
+
+//Emit event(event listner on btn to send message to the server)
+sendBtn.addEventListener('click',function () {
+    socket.emit('chat',{
+        message : message.value,
+        handle : handle.value
+    });
+    message.value="";
+})
